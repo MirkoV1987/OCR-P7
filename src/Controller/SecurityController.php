f5daa1d19901daf2add217234f7786199aec3dc8
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-06-08 23:37:23 +0200

    Upload SecurityController

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Swagger\Annotations as SWG;
+
+
+class SecurityController
+{
+    /**
+     * @Route("/api/login_check", name="login", methods={"POST"})
+     * @SWG\Parameter(
+     *   name="Login",
+     *   description="Fields to provide to sign in and get a token",
+     *   in="body",
+     *   required=true,
+     *   type="string",
+     *   @SWG\Schema(
+     *     type="object",
+     *     title="Login field",
+     *     @SWG\Property(property="email", type="string"),
+     *     @SWG\Property(property="password", type="string")
+     *     )
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="OK",
+     *     @SWG\Schema(
+     *      type="string",
+     *      title="Token",
+     *      @SWG\Property(property="token", type="string"),
+     *     )
+     * )
+     * @SWG\Response(
+     *     response=400,
+     *     description="Bad request - Invalid JSON",
+     * )
+     * @SWG\Response(
+     *     response=401,
+     *     description="Bad credentials",
+     * )
+     * @SWG\Tag(name="Authentication")
+     * @return JsonResponse
+     */
+    public function login() : JsonResponse
+    {
+        $user = $this->getUser();
+
+        return $this->json(array("username" => $user->getUsername(), 'roles' => $user->getRoles() ));
+    }
+}
